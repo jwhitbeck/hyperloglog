jwhitbeck/hyperloglog

commit 83ecbecd48a3e8ac353722c672879efc2dc96bf1

Redis-side observable merging

src/hyperloglog/redis.clj
@@ -30,10 +30,40 @@
   [^long n k]
   (->> (car/hgetall k) (car/parse (partial parse-vec n)) car/with-replies car/return))
 
-(defn mvget
-  "Returns a list of multiple n-element vectors under the provided keys."
+;;; We use a lua script for merging potentially very long lists of observables redis-side.
+(def ^:private map-max-lua-script
+  (->> ["local merged_observable = {}"
+        "for ikey,hkey in ipairs(KEYS) do" ; For each provided key
+        ; Get the raw redis hash. e.g. ["0" "2" "1" "5"]
+        "  local raw_observable = redis.call('hgetall',hkey)"
+        "  local i = 1"
+        "  while i < #raw_observable do"
+        ; We iterate the raw_observable 2 by 2 as it is a flattened list of key/value pairs
+        "    local key = raw_observable[i]"
+        "    local value = raw_observable[i+1]"
+        "    local merged_value = merged_observable[key]"
+        "    if not merged_value or merged_value < value then"
+        "      merged_observable[key] = value"
+        "    end"
+        "    i = i + 2"
+        "  end"
+        "end"
+        ; Format the redis response by flattening all the key/value pairs into a lua array
+        "local redis_response = {}"
+        "for k,v in pairs(merged_observable) do"
+        "  table.insert(redis_response,k)"
+        "  table.insert(redis_response,v)"
+        "end"
+        "return redis_response"]
+       (interpose "\n")
+       (apply str)))
+
+(defn maxmapvget
+  "Retrieves the redis hashes for the provided keys, merges then into a single hash using the 'max' operation
+  on the redis server, and returns the result as a vector of n elements. If an element is missing, its value
+  is set to zero."
   [n & keys]
-  (->> (mapv (partial vget n) keys)
-       (car/with-replies :as-pipeline)
-       (car/parse nil) ; prevent external parsers from leaking into this internal logic
+  (->> (apply car/eval* map-max-lua-script (count keys) keys)
+       car/with-replies
+       (car/parse (partial parse-vec n))
        car/return))

test/hyperloglog/redis_test.clj
@@ -14,17 +14,14 @@
   (wcar* (vset-max "foo" 0 3))
   (-> (wcar* (car/hget "foo" 0)) (#(Integer/parseInt %))) => 5)
 
+(fact "maxmapvget works"
+  (wcar* (car/del "foo" "bar")
+         (car/hmset "foo" 0 1 2 3 4 5)
+         (car/hmset "bar" 0 2 2 1))
+  (wcar* (maxmapvget 10 "foo" "bar")) => [2 0 3 0 5 0 0 0 0 0])
+
 (fact "vget works"
   (wcar* (car/del "foo")
          (car/hset "foo" 0 5)
          (car/hset "foo" 2 10))
   (wcar* (vget 10 "foo")) => [5 0 10 0 0 0 0 0 0 0])
-
-(fact "mvget works"
-  (wcar* (car/del "foo")
-         (car/hset "foo" 0 5)
-         (car/hset "foo" 2 10))
-  (wcar* (car/del "bar")
-         (car/hset "bar" 3 15)
-         (car/hset "bar" 2 10))
-  (wcar* (mvget 10 "foo" "bar")) => [[5 0 10 0 0 0 0 0 0 0] [0 0 10 15 0 0 0 0 0 0]])

src/hyperloglog/time_series.clj
@@ -68,15 +68,15 @@
            (take num-buckets-to-take)
            (mapv (partial time-bucket-prefix (:prefix opts)))))))
 
-(defn fetch-observables-list-for-prefixes
-  "Returns a vector of the hyperloglog num-leading-zeros observables for the provided prefixes. Useful
-  for retrieving observables from several different hyperloglog counters before merging them.
+(defn fetch-observables-for-prefixes
+  "Returns the merged (redis-side) vector of hyperloglog num-leading-zeros observables for the provided
+  prefixes. Useful for retrieving observables from several different hyperloglog counters before merging them.
 
   Takes an optional opts argument that should be the same as the opts map that was used to add the item to the
   hyperloglog counter (see `add-at`)."
   [prefixes & opts]
   (let [opts (apply merge default-opts opts)]
-    (apply redis/mvget (:num-observables opts) prefixes)))
+    (apply redis/maxmapvget (:num-observables opts) prefixes)))
 
 (defn count-latest-distinct
   "Returns the hyperloglog estimate of the number of different items added into the counters over the latest
@@ -86,8 +86,8 @@
   hyperloglog counter (see `add-at`)."
   [seconds & opts]
   (let [opts (apply merge default-opts opts)]
-    (->> (apply redis/mvget (:num-observables opts) (latest-complete-prefixes seconds opts))
-         (car/parse (comp estimate-cardinality (partial apply merge-observables))))))
+    (->> (apply redis/maxmapvget (:num-observables opts) (latest-complete-prefixes seconds opts))
+         (car/parse estimate-cardinality))))
 
 (defn reset
   "Resets all hyperlolog counters pointed at by the optional opts argument. See `add-at` for the available

test/hyperloglog/time_series_test.clj
@@ -34,12 +34,9 @@
 (fact "add-at works"
   (wcar* (reset test-opts))
   (wcar* (add-at "foobar" (minutes-ago 2) test-opts))
-  (->> (wcar* (fetch-observables-list-for-prefixes (latest-complete-prefixes minute test-opts) test-opts))
-       (apply merge-observables))
+  (wcar* (fetch-observables-for-prefixes (latest-complete-prefixes minute test-opts) test-opts))
     => (zero-vec (:num-observables test-opts))
-  (->> (wcar* (fetch-observables-list-for-prefixes (latest-complete-prefixes (* 3 minute) test-opts)
-                                                   test-opts))
-      (apply merge-observables))
+  (wcar* (fetch-observables-for-prefixes (latest-complete-prefixes (* 3 minute) test-opts) test-opts))
     =not=> (zero-vec (:num-observables test-opts))
   ;; Expires are set correctly
   (wcar* (car/ttl (-> (minutes-ago 2)

commit 02d10bb120112928d7ff149799dd5edf15b52229

Fix regression introduced by 83ecbec

project.clj
@@ -1,4 +1,4 @@
-(defproject hyperloglog "0.2.0"
+(defproject hyperloglog "0.2.1"
   :description "A redis-backed hyperloglog implementation in Clojure"
   :url "https://github.com/jwhitbeck/hyperloglog"
   :license {:name "Eclipse Public License"

src/hyperloglog/redis.clj
@@ -42,7 +42,7 @@
         "    local key = raw_observable[i]"
         "    local value = raw_observable[i+1]"
         "    local merged_value = merged_observable[key]"
-        "    if not merged_value or merged_value < value then"
+        "    if not merged_value or tonumber(merged_value) < tonumber(value) then"
         "      merged_observable[key] = value"
         "    end"
         "    i = i + 2"

test/hyperloglog/redis_test.clj
@@ -16,9 +16,9 @@
 
 (fact "maxmapvget works"
   (wcar* (car/del "foo" "bar")
-         (car/hmset "foo" 0 1 2 3 4 5)
+         (car/hmset "foo" 0 10 2 3 4 5)
          (car/hmset "bar" 0 2 2 1))
-  (wcar* (maxmapvget 10 "foo" "bar")) => [2 0 3 0 5 0 0 0 0 0])
+  (wcar* (maxmapvget 10 "foo" "bar")) => [10 0 3 0 5 0 0 0 0 0])
 
 (fact "vget works"
   (wcar* (car/del "foo")