d2dbed23d87b79d6c2dd093cb1fe328417eba8ba
John Whitbeck <john@whitbeck.net>
2017-04-05 21:35:13 -0700

    Remove unused dependency

--- project.clj
+++ project.clj
@@ -9,4 +9,4 @@
                  [org.clojure/clojure "1.5.1"]
                  [org.clojure/math.numeric-tower "0.0.2"]
                  [org.codehaus.jsr166-mirror/jsr166y "1.7.0"] ; Needed for 1.5 reducers
-                 [potemkin "0.3.3"]])
+                 ])
